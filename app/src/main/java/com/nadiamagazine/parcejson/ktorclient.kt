187fb1266efab683fcacc872ed6d4201be518a0c
Nadia Nagazine <nadia.magazine@banno.com>
2021-09-16 14:34:35 -0400

    Changed auth

--- app/src/main/java/com/nadiamagazine/parcejson/ktorclient.kt
+++ app/src/main/java/com/nadiamagazine/parcejson/ktorclient.kt
@@ -1,32 +1,29 @@
 package com.nadiamagazine.parcejson
 
 import io.ktor.client.*
-import io.ktor.client.features.DataConversion.install
-import io.ktor.client.features.auth.*
-import io.ktor.client.features.auth.providers.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlinx.serialization.json.Json
 
 
 suspend fun getInstitutionName(): InstitutionName {
 
     val client = HttpClient {
-            install(Auth) {
-                providers.add(
-                    BasicAuthProvider(
-                        credentials = {
-                            BasicAuthCredentials(
-                                username = "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",
-                                password = "x-oauth-basic"
-                            )
-                        },
-                        sendWithoutRequestCallback = { true }
-                    )
-                )
-            }
+        // Just for an example how I would do authentication for http request
+//            install(Auth) {
+//                providers.add(
+//                    BasicAuthProvider(
+//                        credentials = {
+//                            BasicAuthCredentials(
+//                                username = "",
+//                                password = ""
+//                            )
+//                        },
+//                        sendWithoutRequestCallback = { true }
+//                    )
+//                )
+//            }
         install(JsonFeature) {
             serializer = KotlinxSerializer(
                 kotlinx.serialization.json.Json {
@@ -36,10 +33,10 @@ suspend fun getInstitutionName(): InstitutionName {
             )
             acceptContentTypes = listOf(ContentType.Application.Json, ContentType.Text.Plain)
         }
-        }
+    }
 
     val response: InstitutionName = client
-        .get("https://raw.githubusercontent.com/Banno/grip-assets/master/institutions/5db40b7a-ee76-42b3-acb9-eb7045011c2a/properties.json")
+        .get("https://api.banno.com/institutions/5db40b7a-ee76-42b3-acb9-eb7045011c2a")
     client.close()
     return response
 }
